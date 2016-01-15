d0bcc6b8bcbe77c5de98f2faa7b375f36aaf1cae
Cody Moffitt <codyemoffitt@gmail.com>
2016-01-15 02:27:33 -0600

    Added files
    Added files

---
+++ Day1/authors.pl
@@ -0,0 +1,17 @@
+%% ---
+%%  Day 1: 1 and 2, Authors and Books
+%%  Cody Moffitt
+%% ---
+author(tolkien, the_hobbit).
+author(tolkien, the_lord_of_the_rings).
+author(king, the_gunslinger).
+author(king, pet_sematary).
+author(gaiman, american_gods).
+author(gaiman, sandman).
+author(moore, hellblazer).
+author(moore, swampthing).
+
+%% Code to ask it which books tolkien has written is below, press a to keep getting more books
+%% author(tolkien, What).
+%% 
+
---
+++ Day1/musicians.pl
@@ -0,0 +1,36 @@
+%% ---
+%%  Day 1: 3 and 3, Musicians
+%%  Cody Moffitt
+%% ---
+
+%% Match musicians to instruments %%
+instrument(jimi_hendrix, guitar).
+instrument(david_gilmour, guitar).
+instrument(paco_de_lucia, guitar).
+instrument(geddy_lee, bass).
+instrument(ian_anderson, flute).
+instrument(miles_davis, trumpet).
+instrument(sting, bass).
+instrument(bob_marley, guitar).
+instrument(freddie_mercury, voice).
+instrument(brian_wilson, bass).
+instrument(luciano_pavarotti, flute).
+
+%% Match musicians to genre %%
+genre(jimi_hendrix, rock).
+genre(david_gilmour, rock).
+genre(paco_de_lucia, flamenco).
+genre(geddy_lee, prog_rock).
+genre(ian_anderson, prog_rock).
+genre(miles_davis, acid_jazz).
+genre(sting, new_age).
+genre(sting, rock).
+genre(bob_marley, reggae).
+genre(freddie_mercury, rock).
+genre(brian_wilson, pop_rock).
+genre(luciano_pavarotti, classical).
+
+%% Code to ask it who plays guitar, enter a to keep going
+%% instrument(Who, guitar).
+%% 
+
